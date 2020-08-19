4c96bff64358d1c1991b169b925a38d856db0a31
John W. Terrell <john@alphapixel.com>
2020-08-19 21:41:37 +0100

    Added support for Tracy profilng

---
+++ CMakeModules/FindTracy.cmake
@@ -0,0 +1,41 @@
+# prefer FindTracy from cmake distribution (if it exists)
+if(EXISTS ${CMAKE_ROOT}/Modules/FindTracy.cmake)
+  include(${CMAKE_ROOT}/Modules/FindTracy.cmake)
+
+  if(TRACY_FOUND)
+    return()
+  endif()
+endif()
+
+FIND_PATH(TRACY_INCLUDE_DIR TracyClient/Tracy.hpp
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local/include
+    /usr/include
+    /sw/include # Fink
+    /opt/local/include # DarwinPorts
+    /opt/csw/include # Blastwave
+    /opt/include
+    /usr/freeware/include
+)
+
+FIND_LIBRARY(TRACY_LIBRARY 
+    NAMES TracyClient
+    PATHS
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local/lib
+    /usr/lib
+    /sw/lib
+    /opt/local/lib
+    /opt/csw/lib
+    /opt/lib
+    /usr/freeware/lib64
+    PATH_SUFFIXES
+    lib64
+)
+
+SET(TRACY_FOUND "NO")
+IF(TRACY_LIBRARY AND TRACY_INCLUDE_DIR)
+    SET(TRACY_FOUND "YES")
+ENDIF(TRACY_LIBRARY AND TRACY_INCLUDE_DIR)
